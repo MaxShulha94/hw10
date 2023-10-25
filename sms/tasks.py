172765fbea88c693da43d9308de0d2a5766a0e24
MaxShulha94 <maxshulga94@gmail.com>
2023-10-25 22:56:03 +0300

    first commit

---
+++ sms/tasks.py
@@ -0,0 +1,12 @@
+from celery import shared_task
+from twilio.rest import Client
+from django.conf import settings
+
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+
+@shared_task
+def send_sms(receiver, message):
+    message = client.messages.create(body=message, from_="+16562130708", to=receiver)
+    print(message.sid)
+    return message.sid
---
+++ sms/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .tasks import send_sms
+
+
+def main(request):
+    send_sms.delay("+380957723261", "Hello!")
+    return render(request, "main.html")
