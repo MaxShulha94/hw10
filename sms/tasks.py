911d7c5b84587851d94e2743c9f8952257b60b41
MaxShulha94 <maxshulga94@gmail.com>
2023-10-26 15:36:14 +0300

    third commit

--- sms/tasks.py
+++ sms/tasks.py
@@ -1,14 +1,15 @@
 from celery import shared_task
 from twilio.rest import Client
 from django.conf import settings
+
 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
 
 @shared_task
 def send_sms(receiver, message):
-    message = client.messages \
-        .create(
+    message = client.messages.create(
         body=message,
-        from_='+16562130708',
+        from_="+16562130708",
         to=receiver,
     )
     return message.sid
