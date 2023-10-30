b006298c7251d8aa85acfcc234b506b8e2813d8c
MaxShulha94 <maxshulga94@gmail.com>
2023-10-30 22:08:11 +0200

    third commit

--- sms/tasks.py
+++ sms/tasks.py
@@ -7,9 +7,11 @@ client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
 @shared_task
 def send_sms(receiver, message):
+    print('Go into task!')
     message = client.messages.create(
         body=message,
         from_="+16562130708",
         to=receiver,
     )
+
     return message.sid
--- sms/views.py
+++ sms/views.py
@@ -3,5 +3,10 @@ from .tasks import send_sms
 
 
 def main(request):
-    send_sms.delay("+380957723261", "Hello!")
+    if request.method == 'POST':
+        phone_number = request.POST.get('phone_number')
+        message = "I did it!!!"
+
+        send_sms.delay(phone_number, message)
+
     return render(request, "main.html")
